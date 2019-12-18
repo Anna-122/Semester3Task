83adf1efa880c2e01796766ab2ae4d23c3ad7179
Anna Goncharova <anna.s.goncharova.2001@gmail.com>
2019-12-19 00:24:50 +0200

    work3

--- src/com/company/LingvInterface.java
+++ src/com/company/LingvInterface.java
@@ -5,7 +5,11 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
-
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+import java.util.regex.*;
 
 public class LingvInterface {
     private JTextField Input;
@@ -13,6 +17,7 @@ public class LingvInterface {
     private JPanel MyPanel;
     private JLabel Result;
     private JLabel filename;
+    private JLabel charset;
     private Object Lingvist;
 
 
@@ -20,26 +25,64 @@ public class LingvInterface {
         ClickMe.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed( ActionEvent e ) {
-                Result.setText(Input.getText());
                 JFileChooser fileopen = new JFileChooser();
                 int ret = fileopen.showDialog(null, "Открыть файл");
                 if (ret == JFileChooser.APPROVE_OPTION) {
                     File file = fileopen.getSelectedFile();
-                    filename.setText("/home/anna/IdeaProjects/GOST/61876-71t1.csv");
+//                    filename.setText(file.getName());
+                    String csvFilepath = file.getPath();
+                    filename.setText(csvFilepath);
+                    String line = "";
+                    String cvsSplitBy = ",";
+                    HashMap<String, String> list = new HashMap<>();
+                    try (BufferedReader br = new BufferedReader(new FileReader(csvFilepath))) {
+
+                        while ((line = br.readLine()) != null) {
+
+                            // use comma as separator
+                            String[] country = line.split(cvsSplitBy);
+
+                            //System.out.println(country[0] +"  "  + country[1]);
+                            list.put(country[0], country[1]);
+                        }
+
+                    } catch (IOException err) {
+                        err.printStackTrace();
+                    }
                 }
+//                Result.setText(Input.getText());
+                String textForTranslit = Input.getText();
+                String[] words = textForTranslit.split("\\s");
+                String regex = "[а-яёА-ЯЁ]+";
+                Pattern pattern = Pattern.compile(regex);
+                Matcher m = pattern.matcher(textForTranslit);
+                if (m.find()) {
+                    charset.setText("Исходный набор символов: кириллический");
+                    for (String sub1Text : words) {
+                        if ((sub1Text).charAt(0) > 'А' && (sub1Text.charAt(0) < 'Я')) {
+                            //need to translit kirill->latin
+                        }
+                    }
 
-
+                } else {
+                    charset.setText("Исходный набор символов: латиница");
+                    for (String sub2Text : words) {
+                        if ((sub2Text).charAt(0) > 'A' && (sub2Text.charAt(0) < 'Z')) {
+                            //need to translit latin->kirill
+                        }
+                    }
+                }
             }
         });
     }
 
-//    public static void main( String[] args ) {
-//        JFrame LingvistFrame = new JFrame("LingvistInterface");
-//        LingvistFrame.setContentPane(new LingvInterfice().MyPanel);
-//        LingvistFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//        LingvistFrame.pack();
-//        LingvistFrame.setVisible(true);
-//    }
+    public static void main( String[] args ) {
+        JFrame LingvistFrame = new JFrame("LingvistInterface");
+        LingvistFrame.setContentPane(new LingvInterface().MyPanel);
+        LingvistFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        LingvistFrame.pack();
+        LingvistFrame.setVisible(true);
+    }
 
     public static void showLingvistForm() {
         JFrame LingvistFrame = new JFrame("LingvistInterface");
@@ -66,19 +109,25 @@ public class LingvInterface {
      */
     private void $$$setupUI$$$() {
         MyPanel = new JPanel();
-        MyPanel.setLayout(new com.intellij.uiDesigner.core.GridLayoutManager(3, 2, new Insets(0, 0, 0, 0), -1, -1));
+        MyPanel.setLayout(new com.intellij.uiDesigner.core.GridLayoutManager(6, 2, new Insets(0, 0, 0, 0), -1, -1));
+        MyPanel.setBorder(BorderFactory.createTitledBorder(BorderFactory.createLineBorder(Color.black), null));
         ClickMe = new JButton();
-        ClickMe.setText("Выбрать таблицу из файла");
-        MyPanel.add(ClickMe, new com.intellij.uiDesigner.core.GridConstraints(1, 0, 1, 2, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_CENTER, com.intellij.uiDesigner.core.GridConstraints.FILL_HORIZONTAL, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_SHRINK | com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_GROW, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        ClickMe.setText("Выбрать файл таблицы транслитерации");
+        ClickMe.setToolTipText("Нажмите, чтобы выбрать файл");
+        MyPanel.add(ClickMe, new com.intellij.uiDesigner.core.GridConstraints(1, 0, 3, 2, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_CENTER, com.intellij.uiDesigner.core.GridConstraints.FILL_HORIZONTAL, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_SHRINK | com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_GROW, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, null, new Dimension(30, 20), null, 0, false));
         Result = new JLabel();
         Result.setText("Result");
-        MyPanel.add(Result, new com.intellij.uiDesigner.core.GridConstraints(2, 0, 1, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_WEST, com.intellij.uiDesigner.core.GridConstraints.FILL_NONE, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
-        filename = new JLabel();
-        filename.setText("");
-        MyPanel.add(filename, new com.intellij.uiDesigner.core.GridConstraints(2, 1, 1, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_WEST, com.intellij.uiDesigner.core.GridConstraints.FILL_NONE, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        MyPanel.add(Result, new com.intellij.uiDesigner.core.GridConstraints(5, 0, 1, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_WEST, com.intellij.uiDesigner.core.GridConstraints.FILL_NONE, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
         Input = new JTextField();
         Input.setText("Введите текст для транслитерации");
-        MyPanel.add(Input, new com.intellij.uiDesigner.core.GridConstraints(0, 0, 1, 2, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_WEST, com.intellij.uiDesigner.core.GridConstraints.FILL_HORIZONTAL, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_WANT_GROW, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, new Dimension(2, 2), new Dimension(150, -1), null, 0, false));
+        MyPanel.add(Input, new com.intellij.uiDesigner.core.GridConstraints(0, 0, 1, 2, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_WEST, com.intellij.uiDesigner.core.GridConstraints.FILL_HORIZONTAL, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_WANT_GROW, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, new Dimension(2, 2), new Dimension(200, 20), null, 0, false));
+        charset = new JLabel();
+        charset.setText("Исходный набор символов:");
+        MyPanel.add(charset, new com.intellij.uiDesigner.core.GridConstraints(4, 0, 1, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_WEST, com.intellij.uiDesigner.core.GridConstraints.FILL_NONE, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        filename = new JLabel();
+        filename.setHorizontalAlignment(2);
+        filename.setText("Текущий путь к файлу транслитерации");
+        MyPanel.add(filename, new com.intellij.uiDesigner.core.GridConstraints(3, 0, 1, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_WEST, com.intellij.uiDesigner.core.GridConstraints.FILL_NONE, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
     }
 
     /**
