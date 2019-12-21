f4ee79eaff1c0d8952a542145b683c769f0a288f
Anna Goncharova <anna.s.goncharova.2001@gmail.com>
2019-12-22 00:18:48 +0200

    new method and hashMap inited

--- src/com/khpi/CSVtoMap.java
+++ src/com/khpi/CSVtoMap.java
@@ -1,32 +1,36 @@
 package com.khpi;
 import java.util.regex.*;
-import javax.swing.*;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
 public class CSVtoMap {
+    public static HashMap<String, String> TranslitTableLatinCyr = new HashMap<>();
+    public static HashMap<String, String> TranslitTableCyrLatin = new HashMap<>();
 
     public static void main( String[] args ) throws Exception {
-//        String srcText = "my name Anet Goncharova from Kharkov city";
-        String srcText = "меня зовут Анна Гочарова и я из города Харькова";
-        HashMap<String, String> map = new HashMap<String, String>();
-
+        String srcText = "my name Anet Goncharova from Kharkov city";
+//        String srcText = "меня зовут Анна Гончарова и я из города Харьков";
 
         String line = "";
         String cvsSplitBy = ",";
-        HashMap<String, String> translitmap = new HashMap<>();
         try (BufferedReader br = new BufferedReader(new FileReader("/home/anna/GOST_Translit/61876-71t1.csv"))) {
             while ((line = br.readLine()) != null) {
                 // use comma as separator
                 String[] trans = line.split(cvsSplitBy);
                 System.out.println(trans[0] + "  " + trans[1]);
-                translitmap.put(trans[0], trans[1]);
+                TranslitTableLatinCyr.put(trans[0], trans[1]);
+                TranslitTableCyrLatin.put(trans[1], trans[0]);
             }
 
-            printValues(translitmap);
+//            printValues(TranslitTableLatinCyr);
             Translit(srcText);
         }
     }
@@ -53,6 +57,8 @@ public class CSVtoMap {
                 if (test) {
                     // need to translate
                     System.out.println("Транслитирируем из кирилицы в латиницу слово: " + words[i]);
+                    String word=words[i];
+                    System.out.println("toTranslit имена собственные " + TranslinFromCyrToLat(word));
                 }
             }
         } else {
@@ -61,14 +67,44 @@ public class CSVtoMap {
                 System.out.println(words[i]);
                 boolean test = (words[i]).charAt(0) > 'A' && (words[i].charAt(0) < 'Z');
 //                boolean test = words[i].matches("/^[A-Z][a-z]+$/");
-                    if (test) {
+                if (test) {
                     // need to translate
                     System.out.println("Транслитирируем из латиницы в кирилицу слово: " + words[i]);
+                    String word=words[i];
+                    System.out.println("toTranslit имена собственные " + TranslinFromLatToCyr(word));
                 }
 
-                }
             }
         }
     }
 
 
+        public static String TranslinFromCyrToLat( String word ) {
+            StringBuilder sb = new StringBuilder(word.length());
+            for (int i = 0; i<word.length(); i++) {
+                String l = word.substring(i, i+1);
+                if (TranslitTableCyrLatin.containsKey(l)) {
+                  sb.append(TranslitTableCyrLatin.get(l));
+                }
+                else {
+                    sb.append(l);
+                }
+            }
+            return sb.toString();
+        }
+
+
+        public static String TranslinFromLatToCyr( String word ) {
+            StringBuilder sb = new StringBuilder(word.length());
+            for (int i = 0; i<word.length(); i++) {
+                String l = word.substring(i, i+1);
+                if (TranslitTableLatinCyr.containsKey(l)) {
+                    sb.append(TranslitTableLatinCyr.get(l));
+            }
+                else {
+                    sb.append(l);
+            }
+        }
+            return sb.toString();
+    }
+}
